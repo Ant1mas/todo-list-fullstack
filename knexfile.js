20fc1ab49f011cadecf29c670ec3f32dd34c71ae
Anton Moskovskii <ant1mas.dev@gmail.com>
2024-05-07 22:08:54 +0500

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,16 @@
 const { loadEnvConfig } = require('@next/env')
 
 const dev = process.env.NODE_ENV !== 'production'
-const { PG_URI } = loadEnvConfig('./', dev).combinedEnv
+const { PG_PASSWORD } = loadEnvConfig('./', dev).combinedEnv
 
 module.exports = {
   client: 'pg',
-  connection: PG_URI,
+  connection: {
+    host: '127.0.0.1',
+    user: 'pguser',
+    password: PG_PASSWORD,
+    database: 'postgresdb',
+  },
   migrations: {
     directory: './lib/knex/migrations',
   },
