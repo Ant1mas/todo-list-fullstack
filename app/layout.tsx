66bbd74de2e6b67965e2d460314fb3664510f02b
Anton Moskovskii <ant1mas.dev@gmail.com>
2024-05-11 17:34:27 +0500

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
 import clsx from 'clsx'
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 
 import RootProviders from '@/components/RootProviders'
 import { DEFAULT_METADATA } from '@/lib/config/defaultMetadata'
 import { roboto } from '@/lib/config/fonts'
+import pwaManifest from '@/public/manifest.json'
 import '@/styles/globals.css'
+
+const colorPrimary = pwaManifest.theme_color
 
 export const metadata: Metadata = {
   title: 'Тестовое задание от Московского Антона для ESoft <3',
   ...DEFAULT_METADATA,
+}
+
+export const viewport: Viewport = {
+  themeColor: colorPrimary,
 }
 
 export default function RootLayout({
--- lib/config/defaultMetadata.ts
+++ lib/config/defaultMetadata.ts
@@ -1,3 +1,25 @@
 import type { Metadata } from 'next'
 
-export const DEFAULT_METADATA: Metadata = {}
+import { BASE_PATH } from '@/lib/constants/basePath'
+
+export const DEFAULT_METADATA: Metadata = {
+  manifest: BASE_PATH + '/manifest.json',
+  icons: {
+    icon: [
+      {
+        sizes: '32x32',
+        url: BASE_PATH + '/assets/images/icons/app/favicon.ico',
+      },
+      {
+        type: 'image/svg+xml',
+        sizes: 'any',
+        url: BASE_PATH + '/assets/images/icons/app/favicon.svg',
+      },
+    ],
+    apple: [
+      {
+        url: BASE_PATH + '/assets/images/icons/app/apple-touch-icon.png',
+      },
+    ],
+  },
+}
