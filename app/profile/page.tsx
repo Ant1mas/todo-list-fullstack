5fc071e478b450b1e8bf956c9cac2ba6fc41343f
Anton Moskovskii <ant1mas.dev@gmail.com>
2024-05-11 15:39:49 +0500

    Add styles to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -15,48 +15,62 @@ export default async function page({}: Props) {
   }
 
   return (
-    <div>
-      <div>Профиль</div>
-      <div>
-        <div>
-          <span>Id: </span>
-          <span>{user.id}</span>
-        </div>
-        <div>
-          <span>Логин: </span>
-          <span>{user.login}</span>
-        </div>
-        <div>
-          <span>Имя: </span>
-          <span>{user.name}</span>
-        </div>
-        <div>
-          <span>Фамилия : </span>
-          <span>{user.last_name}</span>
-        </div>
-        <div>
-          <span>Отчество : </span>
-          <span>{user.middle_name}</span>
-        </div>
-        <div>
-          <span>Должность : </span>
-          <span>
-            {user.manager_id !== null ? 'Подчиненный' : 'Руководитель'}
-          </span>
-        </div>
-        {user.manager_id && (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-50 p-5">
+      <div className="rounded-2xl bg-white p-10">
+        <h2 className="pb-4 text-center text-2xl font-light uppercase">
+          Профиль
+        </h2>
+        <div className="my-4 flex flex-col gap-2">
           <div>
-            <span>Id руководителя : </span>
-            <span>{user.manager_id || 'нет'}</span>
+            <span>ID: </span>
+            <span className="font-bold">{user.id}</span>
           </div>
-        )}
+          <div>
+            <span>Логин: </span>
+            <span className="font-bold">{user.login}</span>
+          </div>
+          <div>
+            <span>Имя: </span>
+            <span className="font-bold">{user.name}</span>
+          </div>
+          <div>
+            <span>Фамилия : </span>
+            <span className="font-bold">{user.last_name}</span>
+          </div>
+          <div>
+            <span>Отчество : </span>
+            <span className="font-bold">{user.middle_name}</span>
+          </div>
+          <div>
+            <span>Должность : </span>
+            <span className="font-bold">
+              {user.manager_id !== null ? 'Подчиненный' : 'Руководитель'}
+            </span>
+          </div>
+          {user.manager_id && (
+            <div>
+              <span>ID руководителя : </span>
+              <span className="font-bold">{user.manager_id || 'нет'}</span>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col items-center">
+          <form action={logout}>
+            <button
+              type="submit"
+              className="rounded-2xl bg-red-500 px-4 py-2 text-white duration-150 hover:bg-red-600"
+            >
+              Выйти из аккаунта
+            </button>
+          </form>
+
+          <Link href="/tasks">
+            <button className="mt-2 rounded-2xl bg-gray-500 px-4 py-2 text-white duration-150 hover:bg-gray-600">
+              Перейти на страницу задач
+            </button>
+          </Link>
+        </div>
       </div>
-      <form action={logout}>
-        <button type="submit">Выйти из аккаунта</button>
-      </form>
-      <button>
-        <Link href="/tasks">Перейти на страницу задач</Link>
-      </button>
     </div>
   )
 }
